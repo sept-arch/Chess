37a1779acaa284ca2badb3fc6e6b14db8af60b80
sept-arch <leonardodguzman11@gmail.com>
2025-07-01 21:34:22 -0500

    Fixed the function to create new pieces. I also finished a rough draft of the game::new() function. Board should be a 8x8 2d vector of spaces. Spaces contain piece type, rank, and file. If there is no piece, the space is listed as having a null piece.

--- src/game.rs
+++ src/game.rs
@@ -12,7 +12,7 @@ struct Piece {
 
 impl Piece {
     fn new(color: Color, piece_type: PieceType) -> Piece {
-        Piece {color, false, piece_type}
+        Piece {color, piece_type}
     }
 }
 
@@ -21,6 +21,7 @@ impl Piece {
 enum Color {
     White,
     Black,
+    Null,
 }
 
 enum PieceType {
@@ -65,14 +66,14 @@ pub struct Game {
 impl Game {
     pub fn new() -> Self {
         let columns = HashMap::from([
-            (0, "A"),
-            (1, "B"),
-            (2, "C"),
-            (3, "D"),
-            (4, "E"),
-            (5, "F"),
-            (6, "G"),
-            (7, "H"),
+            (0, 'A'),
+            (1, 'B'),
+            (2, 'C'),
+            (3, 'D'),
+            (4, 'E'),
+            (5, 'F'),
+            (6, 'G'),
+            (7, 'H'),
         ]);
         //Creates the board, first thing to be finally tested in main
         //first, create the pieces
@@ -83,39 +84,44 @@ impl Game {
             board.push(Vec::new());
         }
         //first row
-        board[0].push(Piece::new(White, PieceType::Rook));
-        board[0].push(Piece::new(White, PieceType::Knight));
-        board[0].push(Piece::new(White, PieceType::Bishop));
-        board[0].push(Piece::new(White, PieceType::King));
-        board[0].push(Piece::new(White, PieceType::Queen));
-        board[0].push(Piece::new(White, PieceType::Bishop));
-        board[0].push(Piece::new(White, PieceType::Knight));
-        board[0].push(Piece::new(White, PieceType::Rook));
-        i = 0;
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Rook)), 'A', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Knight)), 'B', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Bishop)), 'C', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::King)), 'D', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Queen)), 'E', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Bishop)), 'F', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Knight)), 'G', 1);
+        board[0].push(Space::new(Piece::new(Color::White, PieceType::Rook)), 'H', 1);
         //second row
         while i < 8 {
             let temp = Piece::new(White, PieceType::Pawn);
-            board[1].push(temp);
+            board[1].push(Space::new(temp, columns[i], 2));
             i += 1;
         }
         //rows 2-5 are empty, will use "null" piece to indicate empty space
+        for n in 2..6 {
+            for k in 0..7 {
+                board[n][k].push(Space::new(Piece::new(Color::Null, PieceType::Null)), columns[i], n + 1);
+            }
+        }
         //Copy and pasted for 7-8, except black
-        //first row
-        board[7].push(Piece::new(White, PieceType::Rook));
-        board[7].push(Piece::new(White, PieceType::Knight));
-        board[7].push(Piece::new(White, PieceType::Bishop));
-        board[7].push(Piece::new(White, PieceType::King));
-        board[7].push(Piece::new(White, PieceType::Queen));
-        board[7].push(Piece::new(White, PieceType::Bishop));
-        board[7].push(Piece::new(White, PieceType::Knight));
-        board[7].push(Piece::new(White, PieceType::Rook));
         i = 0;
-        //second row
         while i < 8 {
-            let temp = Piece::new(White, PieceType::Pawn);
-            board[6].push(temp);
+            let temp = Piece::new(Black, PieceType::Pawn);
+            board[6].push(Space::new(temp, columns[i], 1));
             i += 1;
         }
+        //first row
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Rook)), 'A', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Knight)), 'B', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Bishop)), 'C', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::King)), 'D', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Queen)), 'E', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Bishop)), 'F', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Knight)), 'G', 8);
+        board[7].push(Space::new(Piece::new(Color::Black, PieceType::Rook)), 'H', 8);
+        //second row
+
 
     }
 }
