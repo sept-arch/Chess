5256d2c00717afa02fb59f07c0d32c4dfb305898
sept-arch <leonardodguzman11@gmail.com>
2025-07-08 21:15:31 -0500

    I wrote the game::new() function and struct. I tested to make sure the code would compile and as of right now, the syntax is legal. I am looking into using display and debug for enums to help with debugging. I am going to write a print function that prints a board to terminal purely for testing purposes. Once the logic works, I am going to look into what crates I would have to use to create a full chess application.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-#[derive(Clone)] //easier to duplicate squares
+use std::collections::HashMap;
+
+#[derive(Clone, Eq, Hash, PartialEq)] //easier to duplicate squares
 pub struct Square {
-    pub piece: Piece,
     pub file: char,
     pub rank: u8
 }
 impl Square {
-    pub fn new(piece: Piece, file: char, rank: u8) -> Square {
-        Square { piece, file, rank }
+    pub fn new(file: char, rank: u8) -> Square {
+        Square {file, rank }
     }
 }
 
@@ -23,11 +24,62 @@ impl Piece {
     }
 }
 
+pub struct Game {
+    pub board: HashMap<Square, Option<Piece>>,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        let int_to_char = HashMap::from([
+            (1, 'a'),
+            (2, 'b'),
+            (3, 'c'),
+            (4, 'd'),
+            (5, 'e'),
+            (6, 'f'),
+            (7, 'g'),
+            (8, 'h'),
+        ]);
+        let mut board = HashMap::new();
+        //pawns and null
+        for n in 1..9 {
+            board.insert(Square::new(int_to_char[&n], 2), Piece::new(Color::White, PieceType::Pawn));
+            board.insert(Square::new(int_to_char[&n], 7), Piece::new(Color::Black, PieceType::Pawn));
+            board.insert(Square::new(int_to_char[&n], 3), None);
+            board.insert(Square::new(int_to_char[&n], 4), None);
+            board.insert(Square::new(int_to_char[&n], 5), None);
+            board.insert(Square::new(int_to_char[&n], 6), None);
+        }
+        //white back file
+        board.insert(Square::new('a', 1), Piece::new(Color::White, PieceType::Rook));
+        board.insert(Square::new('h', 1), Piece::new(Color::White, PieceType::Rook));
+        board.insert(Square::new('b', 1), Piece::new(Color::White, PieceType::Knight));
+        board.insert(Square::new('g', 1), Piece::new(Color::White, PieceType::Knight));
+        board.insert(Square::new('c', 1), Piece::new(Color::White, PieceType::Bishop));
+        board.insert(Square::new('f', 1), Piece::new(Color::White, PieceType::Bishop));
+        board.insert(Square::new('e', 1), Piece::new(Color::White, PieceType::King));
+        board.insert(Square::new('d', 1), Piece::new(Color::White, PieceType::Queen));
+        //black back file
+        board.insert(Square::new('a', 8), Piece::new(Color::Black, PieceType::Rook));
+        board.insert(Square::new('h', 8), Piece::new(Color::Black, PieceType::Rook));
+        board.insert(Square::new('b', 8), Piece::new(Color::Black, PieceType::Knight));
+        board.insert(Square::new('g', 8), Piece::new(Color::Black, PieceType::Knight));
+        board.insert(Square::new('c', 8), Piece::new(Color::Black, PieceType::Bishop));
+        board.insert(Square::new('f', 8), Piece::new(Color::Black, PieceType::Bishop));
+        board.insert(Square::new('e', 8), Piece::new(Color::Black, PieceType::King));
+        board.insert(Square::new('d', 8), Piece::new(Color::Black, PieceType::Queen));
+    Game {
+       board,
+        }
+    }
+    //only for testing purposes
+}
+
 #[derive(Clone)]
 pub enum Color {
     White,
     Black,
-    Null,
+    //Null,
 }
 
 #[derive(Clone)]
@@ -38,8 +90,10 @@ pub enum PieceType {
     Bishop,
     Queen,
     King,
-    Null,
+    //Null,
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -48,4 +102,12 @@ mod tests {
     fn make_piece() {
         Piece::new(Color::Black, PieceType::Pawn);
     }
+    #[test]
+    fn make_square() {
+        Square::new('D', 4);
+    }
+    #[test]
+    fn create_board() {
+        Game::new();
+}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-//use Chess::*;
+use Chess::*;
 //Uncomment later; have it uncommented so compiler does not complain
 
 fn main() {
     //TODO: write main lol
+    //psuedo-code setup:
+    //run the game::new() function,
+    //run game::run() which returns the winner
+    //bring up game over screen declaring winner
+    //option to play again, or exit application
 }
